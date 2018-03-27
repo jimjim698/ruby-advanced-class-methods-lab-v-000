e62c772257310bde7cc3a54691c819b0708853aa
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-27 20:59:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,4 +16,15 @@ class Song
     self.class.all << self
   end
 
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
+end
+
+def self.create_by_name(name)
+  song = self.new
+  song.name = name 
+  song.save
+  song
 end
